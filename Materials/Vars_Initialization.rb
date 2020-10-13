9fb03bc855d3179d4ba88e79cca449069774366f
stesproject <stefmercadante@gmail.com>
2020-10-13 23:52:17 +0200

    added new var initialization

--- Materials/Vars_Initialization.rb
+++ Materials/Vars_Initialization.rb
@@ -4,6 +4,9 @@ class Vars_Initialization
     $game_variables[99] = 255
     $game_variables[100] = 28
 
+    # puntata arena (map32)
+    $game_variables[102] = 20
+
     # Disable default menu
     $game_system.menu_disabled = true
   end

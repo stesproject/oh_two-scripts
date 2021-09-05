c2979fa2818d0c4ac56ad10ee914e6a8495f6f12
Mercadante Stefano <s.mercadante@reply.it>
2021-09-05 21:29:30 +0200

    update credits

--- Materials/Vars_Initialization.rb
+++ Materials/Vars_Initialization.rb
@@ -20,5 +20,33 @@ class Vars_Initialization
       "WORATANA (NeoSave system, Message system)", 
       "ALLY (Jump system)"
     ]
+
+    # Credits (music)
+    $credits_music_1 = [
+      "ENRICO CASCAVILLA (Main Title)", 
+      "JASPRELAO (Memories)", 
+      "RPGMAKER 3 MUSIC PACK (The Beginning)", 
+      "MIGHT BE MAGIC (Green Forest)", 
+      "XCALNAROK (Castle Grounds)", 
+      "ENRICO CASCAVILLA (Dungeons)", 
+      "RAFFAELE MURRI (Wild Valley)",
+      "GAMESFREAK13563 (Rocky Mountains)", 
+      "LANA42 (Dead Valley)", 
+      "BENSOUND.COM (Abyssal Waterfalls)", 
+      "RPGMAKER 3 MUSIC PACK (Forest)", 
+    ]
+    $credits_music_2 = [
+      "JUDE (Cyberspace)", 
+      "AARON KROGH (Suburbs)", 
+      "EMANUEL (Race)",
+      "SNOWY FOX (Ancient Ruins)", 
+      "AARON KROGH (Against the General)", 
+      "TARRANON (Ruins Collapse)", 
+      "TARRANON (Towards Derijiro)", 
+      "WAVEBRIGADE (Standard Battle)", 
+      "AUDIONAUTIX (Boss Battle)", 
+      "DIBUR (Final Battle)",
+      "MIGHT BE MAGIC (The End)"
+    ]
   end
 end
